d51889eada393707afb99ce52022d4ffec2a56d3
Donald <donaldl@mit.edu>
2021-01-27 02:07:30 -0800

    Added in a function to ignore barriers

--- test/basicplayer/RobotTest.java
+++ test/basicplayer/RobotTest.java
@@ -1,7 +1,15 @@
 package basicplayer;
 
 import junit.framework.TestCase;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class RobotTest extends TestCase {
+
+    @Test
+    public void testSanity() {
+        assertEquals(2, 1+1);
+    }
 
 }
